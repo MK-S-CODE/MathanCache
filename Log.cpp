c267503efdb2e27580c6d67dbf685e1963c8419f
MK-S-CODE <mathankumar3112s@gmail.com>
2026-09-17 18:23:35 +0530

    cleaned up project folder

--- Log.cpp
+++
@@ -1,6 +0,0 @@
-#include "Log.h"
-
-void FetchAndPush(RingBuffer* buffer)  {
-    int reading = 25;
-    buffer->push(reading);
-}
--- common.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-#include "log.h"
-#include<string>
-int GetLogReading();
